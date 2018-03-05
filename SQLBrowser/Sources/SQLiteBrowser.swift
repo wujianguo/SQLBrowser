ce2b8a612a9ef529ca7720361c0f17293039ba38
wujianguo <wujianguo@haixue.com>
2018-03-05 16:49:24 +0800

    gridview

--- SQLBrowser/Sources/SQLiteBrowser.swift
+++ SQLBrowser/Sources/SQLiteBrowser.swift
@@ -28,6 +28,227 @@ struct SQLiteDatabase {
     }
 }
 
+
+class GridViewCell: UIView {
+    var isSelected: Bool = false
+    
+    var isHighlighted: Bool = false
+    
+    var textLabel: UILabel!
+    
+    func prepareForReuse() {
+        
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupUI() {
+        textLabel = UILabel()
+        addSubview(textLabel)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        textLabel.frame = bounds
+    }
+}
+
+class GridView: UIScrollView, UITableViewDataSource, UITableViewDelegate{
+    
+    weak var gridViewDelegate: GridViewDelegate?
+    
+    weak var dataSource: GridViewDataSource?
+
+    func dequeueReusableCell(for column: Int) -> GridViewCell {
+        return GridViewCell()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .plain)
+        tableView.delegate = self
+        tableView.dataSource = self
+        return tableView
+    }()
+    
+    class InternalTableViewCell: UITableViewCell {
+        
+        static let identifier = "InternalTableViewCellIdentifier"
+        
+        override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+            super.init(style: style, reuseIdentifier: reuseIdentifier)
+        }
+        
+        required init?(coder aDecoder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+        
+        var labels: [UILabel] = [UILabel]()
+        var lines: [UIView] = [UIView]()
+        var widths = [CGFloat]() {
+            didSet {
+                setupUIIfNeed()
+            }
+        }
+        
+        var texts: [String] = [String]() {
+            didSet {
+                if labels.count != texts.count {
+                    return
+                }
+                for i in 0..<labels.count {
+                    labels[i].text = texts[i]
+                }
+            }
+        }
+
+        func setupUIIfNeed() {
+            if labels.count == widths.count {
+                return
+            }
+            for label in labels {
+                label.removeFromSuperview()
+            }
+            labels.removeAll()
+            var x: CGFloat = 0
+            for i in 0..<widths.count {
+                let label = UILabel()
+                label.frame = CGRect(x: x, y: 0, width: widths[i], height: bounds.height)
+                contentView.addSubview(label)
+                labels.append(label)
+                
+                if i > 0 {
+                    let line = UIView()
+                    line.frame = CGRect(x: x, y: 0, width: 1, height: bounds.height)
+                    line.backgroundColor = UIColor.lightGray
+                    contentView.addSubview(line)
+                    lines.append(line)
+                }
+                
+                x += widths[i]
+            }
+            doLayout()
+        }
+        
+        func doLayout() {
+            if labels.count != widths.count {
+                return
+            }
+            var x: CGFloat = 0
+            for i in 0..<widths.count {
+                labels[i].frame = CGRect(x: x, y: 0, width: widths[i], height: bounds.height)
+                if i > 0 {
+                    lines[i-1].frame = CGRect(x: x, y: 0, width: 1, height: bounds.height)
+                }
+                x += widths[i]
+            }
+        }
+        
+        override func layoutSubviews() {
+            super.layoutSubviews()
+            doLayout()
+        }
+    }
+    
+    func setupUI() {
+        addSubview(tableView)
+        tableView.separatorInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        tableView.register(InternalTableViewCell.self, forCellReuseIdentifier: InternalTableViewCell.identifier)
+//        tableView.translatesAutoresizingMaskIntoConstraints = false
+//        NSLayoutConstraint.activate([
+//            tableView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+//            tableView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor),
+//            tableView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor),
+//            tableView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor)
+//            ])
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        let width = max(contentWidth, bounds.width)
+        tableView.frame = CGRect(x: 0, y: 0, width: width, height: bounds.height)
+        contentSize = CGSize(width: width, height: bounds.height)
+    }
+    
+    lazy var contentWidth: CGFloat = {
+        return widths.reduce(0, +)
+    }()
+    
+    lazy var widths: [CGFloat] = {
+        var w = [CGFloat]()
+        let columns = dataSource?.numberOfColumns(in: self) ?? 0
+
+        for i in 0..<columns {
+            w.append(gridViewDelegate?.gridView(self, widthForColumn: i) ?? 0)
+        }
+        return w
+    }()
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataSource?.numberOfRows(in: self) ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: InternalTableViewCell.identifier, for: indexPath) as! InternalTableViewCell
+        cell.widths = widths
+        var texts = [String]()
+        for i in 0..<widths.count {
+            texts.append(dataSource?.gridView(self, textForCellAt: IndexPath(row: i, section: indexPath.row)) ?? "")
+        }
+        cell.texts = texts
+        cell.selectionStyle = .none
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        
+    }
+    
+    func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        
+    }
+}
+
+protocol GridViewDataSource: NSObjectProtocol {
+    
+    func numberOfRows(in gridView: GridView) -> Int
+
+    func numberOfColumns(in gridView: GridView) -> Int
+    
+    func gridView(_ gridView: GridView, textForCellAt indexPath: IndexPath) -> String    
+}
+
+protocol GridViewDelegate: NSObjectProtocol {
+    func gridView(_ gridView: GridView, didHighlightRowAt indexPath: IndexPath)
+    func gridView(_ gridView: GridView, didUnhighlightRowAt indexPath: IndexPath)
+    
+    func gridView(_ gridView: GridView, didSelectRowAt indexPath: IndexPath)
+    func gridView(_ gridView: GridView, didDeselectRowAt indexPath: IndexPath)
+
+    func gridView(_ gridView: GridView, heightForRowAt indexPath: IndexPath) -> CGFloat
+    func gridView(_ gridView: GridView, widthForColumn column: Int) -> CGFloat
+}
+
+
 class SelectStatementViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
--- SQLBrowser/ViewController.swift
+++ SQLBrowser/ViewController.swift
@@ -8,11 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, GridViewDataSource, GridViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(rightBarButtonItemClick(sender:)))
+        
+        let gridView = GridView(frame: .zero)
+        gridView.gridViewDelegate = self
+        gridView.dataSource = self
+        view.addSubview(gridView)
+        gridView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            gridView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            gridView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            gridView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            gridView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
+            ])
+
+ /*
+        let testView = UIView()
+        testView.backgroundColor = UIColor.red
+        testView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(testView)
+        NSLayoutConstraint.activate([
+            testView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            testView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            testView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            testView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
+            ])
+*/
     }
     
     @objc func rightBarButtonItemClick(sender: UIBarButtonItem) {
@@ -21,11 +47,40 @@ class ViewController: UIViewController {
         present(nav, animated: true, completion: nil)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func numberOfRows(in gridView: GridView) -> Int {
+        return 100
     }
-
-
+    
+    func numberOfColumns(in gridView: GridView) -> Int {
+        return 20
+    }
+    
+    func gridView(_ gridView: GridView, textForCellAt indexPath: IndexPath) -> String {
+        return "\(indexPath.section),\(indexPath.row)"
+    }
+    
+    func gridView(_ gridView: GridView, widthForColumn column: Int) -> CGFloat {
+        return 100
+    }
+    
+    func gridView(_ gridView: GridView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 40
+    }
+    
+    func gridView(_ gridView: GridView, didHighlightRowAt indexPath: IndexPath) {
+        
+    }
+    
+    func gridView(_ gridView: GridView, didUnhighlightRowAt indexPath: IndexPath) {
+        
+    }
+    
+    func gridView(_ gridView: GridView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    
+    func gridView(_ gridView: GridView, didDeselectRowAt indexPath: IndexPath) {
+        
+    }
 }
 
